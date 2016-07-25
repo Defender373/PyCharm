0acd9d581474c1eb54a02d042f9b891f8aa78249
Greg <gregory9797@gmail.com>
2016-07-25 11:22:09 -0500

    Test3

--- Euler problem #11.py
+++ Euler problem #11.py
@@ -4,3 +4,15 @@ Euler = [[-1] * 20 for x in range(20)]
 
 Euler[15][12] = 20
 print(Euler[15][12])
+f = open('Euler11numbers.txt', 'r')
+
+print(f.readline())
+# f.readline()[4] gets the 4th index
+
+# for x in range 20  for each row
+i = 0
+# substring from i to i+1 then add 3 to i. i += 3. Then repeat
+
+int()  # make sure to cast the strings to ints
+
+# Euler[x][i] = substring i to i+1
