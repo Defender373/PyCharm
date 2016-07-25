39fa62ec4241753dba5638e120f23bcfc7e1f72b
slcc2c <spork76@gmail.com>
2016-07-25 11:45:05 -0500

    Added up and left

--- Euler problem #11.py
+++ Euler problem #11.py
@@ -3,7 +3,7 @@
 Euler = [[-1] * 20 for x in range(20)]
 
 Euler[15][12] = 20
-print(Euler[15][12])
+#print(Euler[][12])
 f = open('Euler11numbers.txt', 'r')
 
 print(f.readline())
@@ -15,3 +15,44 @@ print(f.readline())
     # for y in range(0,20,3) counts from 0 to 20 by 3's allowing us to skip spaces
 
         # Euler[x][i] = substring i to i+1 cast to an int; int()
+
+# Idea, build functions to calculate the product in each of the 8 directions, I've done up and left
+
+#call up on Euler[10][11], up(Euler, 10, 11)
+#call left on Euler[10][11], left(Euler[10], 11)
+
+def up(self, array, x_index, y_index):
+    return array[y_index][x_index]*array[y_index+1][x_index]*array[y_index+2][x_index]*array[y_index+3][x_index]
+
+def down(self, array, x_index, y_index):
+    product = 0
+
+    return product
+
+def left(self, array, x_index):
+    return array[x_index]*array[x_index-1]*array[x_index-2]*array[x_index-3]
+
+def right(self, array, x_index):
+    product = 0
+
+    return product
+
+def up_left(self, array, x_index, y_index):
+    product = 0
+
+    return product
+
+def up_right(self, array, x_index, y_index):
+    product = 0
+
+    return product
+
+def down_left(self, array, x_index, y_index):
+    product = 0
+
+    return product
+
+def down_right(self, array, x_index, y_index):
+    product = 0
+
+    return product
